e68eca76cba65330ee2ba45b3e7fbd8a208a46cb
matsixfive <maffers2006@gmail.com>
2022-06-21 14:48:42 +0100

    app rename

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import { AppProps } from "next/app";
+
 import { ThemeWrapper } from "../context/darkMode";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeWrapper>
       <Component {...pageProps} />
